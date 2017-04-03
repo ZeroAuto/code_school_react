3f0527fd0e1f47cada8f06ac116c8ff9acc66fbd
Mike James <mikejames386@gmail.com>
2017-04-03 18:27:54 -0500

    level 1 - First Component 1.7

--- component.js
+++ component.js
@@ -1,6 +1,29 @@
+// JSX is Not a string
+
+// 'class' is a reserved in Javascript
+// so JSX has a different way to express
+// HTML class attributes
+
+// Any code written in curly braces {} is interpreted as literal Javascript
+
+// elements in upper camel case are rendered as react components
+
 class StoryBox extends React.Component {
   render() {
-    return ( <div>Story Box</div> );
+    const now = new Date();
+
+    const topicsList = ["HTML", "Javascript", "React"];
+
+    return(
+      <div>
+        <h1>Stories App</h1>
+        <p className="lead">Current Time: {now.toTimeString()}</p>
+
+        <ul>
+          {topicsList.map( topic => <li>{topic}</li> )}
+        </ul>
+      </div>
+    );
   }
 }
 
