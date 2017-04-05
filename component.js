00b07efc84b6fcf69fd3558bbec1a0bfa6f595e6
Mike James <mikejames386@gmail.com>
2017-04-05 17:59:02 -0500

    Level 3 - Wrap Up

--- component.js
+++ component.js
@@ -68,20 +68,43 @@ class CommentBox extends React.Component {
 }
 
 class Comment extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isAbusive: false
+    }
+  }
   render() {
+    let commentBody;
+
+    if (!this.state.isAbusive) {
+      commentBody = this.props.body;
+    } else {
+      commentBody = <em>Content marked as abusive</em>;
+    }
+
     return(
       <div className="comment">
         <p className="comment-header">{this.props.author}</p>
         <p className="comment-body">
-          {this.props.body}
+          {commentBody}
         </p>
         <div className="comment-footer">
           <a href="#" className="comment-footer-delete">
             Delete Comment
           </a>
+          <a href="#" onClick={this._toggleAbuse.bind(this)}>Report as Abuse</a>
         </div>
       </div>
     );
+  }
+
+  _toggleAbuse(event) {
+    event.preventDefault();
+
+    this.setState({
+      isAbusive: !this.state.isAbusive
+    })
   }
 }
 
