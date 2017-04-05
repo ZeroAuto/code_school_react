159fbc32f9f6b8ed3aca2e70915f03fa15ae2dbb
Mike James <mikejames386@gmail.com>
2017-04-05 12:13:07 -0500

    Level 2.1 - Building an App

--- component.js
+++ component.js
@@ -8,25 +8,42 @@
 
 // elements in upper camel case are rendered as react components
 
-class StoryBox extends React.Component {
+class CommentBox extends React.Component {
   render() {
-    const now = new Date();
-
-    const topicsList = ["HTML", "Javascript", "React"];
 
     return(
-      <div>
-        <h1>Stories App</h1>
-        <p className="lead">Current Time: {now.toTimeString()}</p>
+      <div className="comment-box">
+      <h3>Comments</h3>
+      <h4 className="comment-count">2 comments</h4>
+        <div className="comment-list">
+          <Comment
+            author="Morgan McCircuit" body="Great picture!" />
+          <Comment
+            author="Bending Bender" body="Excellent stuff" />
+        </div>
+      </div>
+    );
+  }
+}
 
-        <ul>
-          {topicsList.map( topic => <li>{topic}</li> )}
-        </ul>
+class Comment extends React.Component {
+  render() {
+    return(
+      <div className="comment">
+        <p className="comment-header">{this.props.author}</p>
+        <p className="comment-body">
+          {this.props.body}
+        </p>
+        <div className="comment-footer">
+          <a href="#" className="comment-footer-delete">
+            Delete Comment
+          </a>
+        </div>
       </div>
     );
   }
 }
 
 ReactDOM.render(
-  <StoryBox />, document.getElementById('story-app')
+  <CommentBox />, document.getElementById('story-app')
 );
